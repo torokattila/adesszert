f305f53dc0838802c41cd298b46fe73d2881a128
Török Attila <torcsiattila93@gmail.com>
2020-09-15 22:10:53 +0200

    refactor(footer): modified css style on footer

--- controller/scripts.js
+++ controller/scripts.js
@@ -69,26 +69,34 @@ $(document).ready(function() {
 
     if (window.location.href.indexOf('main') > -1) {
         $('#main-page').css('color', '#e47277');
+        $('.footer-main').css('color', '#e47277');
     } else {
         $('#main-page').css('color', '#5e3c3e');
+        $('.footer-main').css('color', 'whitesmoke');
     }
 
     if (window.location.href.indexOf('cakes') > -1) {
         $('#cakes-page').css('color', '#e47277');
+        $('.footer-cakes').css('color', '#e47277');
     } else {
         $('#cakes-page').css('color', '#5e3c3e');
+        $('.footer-cakes').css('color', 'whitesmoke');
     }
 
     if (window.location.href.indexOf('order') > -1) {
         $('#order-page').css('color', '#e47277');
+        $('.footer-order').css('color', '#e47277');
     } else {
         $('#order-page').css('color', '#5e3c3e');
+        $('.footer-order').css('color', 'whitesmoke');
     }
 
     if (window.location.href.indexOf('contact') > -1) {
         $('#contact-page').css('color', '#e47277');
+        $('.footer-contact').css('color', '#e47277');
     } else {
         $('#contact-page').css('color', '#5e3c3e');
+        $('.footer-contact').css('color', 'whitesmoke');
     }
 
     $('.contact-send-button').on('click', function(event) {
